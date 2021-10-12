de81d40d74107a37545cf71e4205a2c51cc0c4fb
Simran Sandhu <simran7s@my.yorku.ca>
2021-10-12 17:19:20 -0400

    completed project

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const sounds = ["applauces", "boo", "gasp", "tada", "victory", "wrong"]
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"]
+const emojis = ["👏", "👎", "😲", "🎉", "🏆", "❌"]
 
-sounds.forEach(sound => {
+sounds.forEach((sound, index) => {
     const btn = document.createElement("button")
     btn.classList.add("btn")
 
-    btn.innerText = sound
+    btn.innerText = sound + " " + emojis[index]
 
+    btn.addEventListener("click", () => {
+        //Stop all other sounds
+        stopSongs()
+
+        // Play audio with that ID
+        document.getElementById(sound).play()
+    })
     // Put the create button within the buttons div
     document.getElementById("buttons").appendChild(btn)
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+        song.pause();
+        // reset to beginning of the audio
+        song.currentTime = 0;
+    })
+}
